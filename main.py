bcd469804be6ddafec98b6b5c27f1908b6242205
Mason <43713560+mercurrie@users.noreply.github.com>
2023-02-22 20:48:45 -0600

    refactor
    included function to handle get requests.
    called function as needed within route functions

--- main.py
+++ main.py
@@ -8,22 +8,27 @@ BLOG_API = "https://api.npoint.io/c790b4d5cab58020d391"
 HOME_PATH = '/'
 POST_ID_KEY = "id"
 SHOW_POST_PATH = "/path/<int:index>"
-blog_response = requests.get(BLOG_API)
-blog_posts = blog_response.json()
 
 
 @app.route(HOME_PATH)
 def home():
+    blog_posts = blog_response()
     return render_template(INDEX_HTML, posts=blog_posts)
 
 
 @app.route(SHOW_POST_PATH)
 def show_post(index):
     requested_post = None
+    blog_posts = blog_response()
+
     for post in blog_posts:
         if index == post[POST_ID_KEY]:
             requested_post = post
     return render_template(POST_HTML, blog_post=requested_post)
+
+
+def blog_response():
+    return requests.get(BLOG_API).json()
 
 
 if __name__ == "__main__":
